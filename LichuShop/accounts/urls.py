11835b3cb3fabfcf591513ae2a501a57da8858dd
mdrion12 <reon128633@gmal.com>
2026-02-14 11:41:09 +0600

    logout view added

--- LichuShop/accounts/urls.py
+++ LichuShop/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register,login
+from .views import register,login,logout
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -15,4 +15,5 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/',register, name='register'),
     path('login/',login,name="login"),
+    path('logout/',logout,name='logout'),
 ]
--- LichuShop/accounts/views.py
+++ LichuShop/accounts/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from .serializers import UserSerializer,ResetPasswordSerializer,loginSerializer
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from .models import user
 
 @api_view(['POST'])
 def register(request):
+    
     serializer=UserSerializer(data=request.data)
-    if serializer.is_valid:
+    
+    if user.objects.exist():
+        return Response({"message":"user already exist! try to another way"},status=status.HTTP_400_BAD_REQUEST)
+    if serializer.is_valid():
         serializer.save()
-        return Response({"message":"user created successfully"},status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "User created successfully"},status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  
 
 @api_view(['POST'])
@@ -30,4 +36,15 @@ def login(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try:
+        refresh=request.data.get("refresh")
+        if not refresh:
+              return Response({"error": "Refresh token missing"}, status=status.HTTP_400_BAD_REQUEST)
+        refresh_token=RefreshToken(refresh)
+        refresh_token.blacklist()
+        return Response({"message": "Logout successful"},status=status.HTTP_205_RESET_CONTENT)
+    except Exception as e:
+        return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
